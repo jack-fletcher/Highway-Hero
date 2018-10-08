21dd4f568c57cc9541df39141f6412f18cad1acd
10203040506070809 <33032000+10203040506070809@users.noreply.github.com>
2018-10-08 10:55:33 +0100

    Added plane, car and basic movement

---
+++ AINT254-Project1/Assets/Scripts/movementScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour {
+
+    public float force = 3000;
+    public Rigidbody car;
+	// Use this for initialization
+	void Start () {
+
+        car = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Debug.Log("A thing was done!");
+            car.AddForce(Vector3.left * force);
+            
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            Debug.Log("A thing was done!");
+            car.AddForce(Vector3.right * force);
+
+        }
+    }
+}
