c9c1037777142ff22149cf7900b57d0f08925527
10203040506070809 <33032000+10203040506070809@users.noreply.github.com>
2018-10-12 15:54:09 +0100

    Session work updated

---
+++ week2session2/Assets/Scripts/customInput.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class customInput : MonoBehaviour {
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 200, 30), ("Horizontal Axis: " + Input.GetAxis("Horizontal")));
+        GUI.Label(new Rect(10, 30, 200, 30), ("Vertical Axis: " + Input.GetAxis("Vertical")));
+        
+    }
+}
+
