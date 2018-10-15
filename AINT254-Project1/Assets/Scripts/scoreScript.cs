a2671c3cc28809bb81f2a6c83b315440d9daf7af
10203040506070809 <33032000+10203040506070809@users.noreply.github.com>
2018-10-15 02:12:49 +0100

    Changed movement script, fixed flying bug, added basic terrain/obstacles

---
+++ AINT254-Project1/Assets/Scripts/scoreScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreScript : MonoBehaviour {
+    private float timer;
+    public static int score;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer > 1f)
+        {
+            score += 1;
+
+            timer = 0;
+        }
+    }
+}
